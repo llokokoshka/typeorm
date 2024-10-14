b7f8dbd78af92692a0726af07387d6fcaabc63fa
Katerina <katelokota@gmail.com>
2024-10-14 13:21:05 +0300

    add crypto

--- src/controllers/appControllers.ts
+++ src/controllers/appControllers.ts
@@ -1,28 +1,41 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 import { Request, Response } from "express";
-const bcrypt = require("bcrypt");
 import { User } from "../entity/User";
 import { AppDataSource } from "../index";
+const crypto = require("crypto")
 
 const userRepository = AppDataSource.getRepository(User);
 
 export function generateAccessToken(id) {
-  return jwt.sign(id, process.env.TOKEN_SECRET, { expiresIn: '3h' });
+  return jwt.sign(id, process.env.TOKEN_SECRET, { expiresIn: '1h' });
 }
+
+function generatePassword(password: string) {
+  const salt = crypto.randomBytes(32).toString('hex')
+  const genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex')
+  return {
+    salt: salt,
+    hash: genHash
+  }
+}
+
+function validPassword(password, hash, salt) {
+  const checkHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex')
+  return hash === checkHash
+}
+
 
 export const registration = async (req: Request, res: Response) => {
   if (!req.body) return res.sendStatus(400);
   try {
-    
-    const salt = await bcrypt.genSalt(10);
     const { fullName, email, password, Dob } = req.body;
-    const passwordHash = await bcrypt.hash(password, salt);
+    const hashPass = generatePassword(password);
     const user = new User();
     user.fullName = fullName;
     user.email = email;
     user.Dob = Dob;
-    user.password = passwordHash;
+    user.password = hashPass.salt + '//' + hashPass.hash;
     await userRepository.save(user);
     const token = generateAccessToken({ id: req.body.id });
     console.log("user are addited");
@@ -37,12 +50,13 @@ export const login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const user = await userRepository.findOneBy({ email: email });
     if (!user) return;
-    bcrypt.compare(password, user.password, function (err, result) {
-      if (result) {
-        const token = generateAccessToken({ id: req.body.id });
-        res.json({ user, token });
-      }
-    });
+    const salt = user.password.split('//')[0];
+    const userHashPassword = user.password.split('//')[1];
+    const checkPass = validPassword(password, salt, userHashPassword);
+    if (checkPass) {
+      const token = generateAccessToken({ id: req.body.id });
+      res.json({ user, token });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).send("Error while loggin user");
@@ -66,13 +80,12 @@ export const updateUser = async (req: Request, res: Response) => {
   try {
     const user = await userRepository.findOneById(req.params.id);
     if (!user) return res.status(500).send("User not found");
-    const salt = await bcrypt.genSalt(10);
     const { name, email, password, Dob } = req.body;
-    const passwordHash = await bcrypt.hash(password, salt);
+    const hashPass = generatePassword(password);
     user.fullName = name;
     user.email = email;
     user.Dob = Dob;
-    user.password = passwordHash;
+    user.password = hashPass.salt + '//' + hashPass.hash;
     await userRepository.save(user);
     console.log("user are changed");
     res.json(user);
